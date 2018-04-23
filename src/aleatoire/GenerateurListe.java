7ca119de6d26a8e56aeb8e84b162039aed49c0fd
Grillette <guilhem.s@protonmail.com>
2018-04-24 01:47:08 +0200

    liste de nombre aleatoire pour la loi uniforme

--- src/aleatoire/GenerateurListe.java
+++ src/aleatoire/GenerateurListe.java
@@ -1,5 +1,30 @@
 package aleatoire;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenerateurListe {
+	public static int nbSimulation = 1000;
+	
+	
+	public static void main(String[] args) {
+		
+		List<Double> listeUniforme = listeNbAleatoireLoiUniforme();
+		
+		for (int i = 0; i<listeUniforme.size(); i++) {
+			System.out.println(listeUniforme.get(i));
+		}
+	}
+	
+	
+	public static List<Double> listeNbAleatoireLoiUniforme() {
+		List<Double> listeAleat = new ArrayList<>();
+		
+		for (int i = 0; i<nbSimulation; i++) {
+			listeAleat.add(Double.valueOf(GenerateurAleatoire.generateurAleatoire()));
+		}
+		
+		return listeAleat;
+	}
 	
 }
